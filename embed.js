4b75b02fbe0f0f745cad5afc7231f220e2376af1
elijahchen <code.elijahchen@gmail.com>
2017-03-29 14:55:16 -0400

    - Tested one:many theory, confirmed working

--- embed.js
+++ embed.js
@@ -2,43 +2,55 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://user:testapp@ds129050.mlab.com:29050/ecdata");
 
-// User - email, name
-let userSchema = new mongoose.Schema({
-    email: String,
-    name: String
-});
-let userModel = mongoose.model("aUser", userSchema);
-
 // Post - title, content
 let postSchema = new mongoose.Schema({
     title: String,
     post: String
 });
-let postModel = mongoose.model("aPost", postSchema);
+let Post = mongoose.model("aPost", postSchema);
 
-// let newUser = new userModel({
-//     email: "Trucker@aaatruck.com",
-//     name: "Tuck Ering"
+// User - email, name, post schema
+let userSchema = new mongoose.Schema({
+    email: String,
+    name: String,
+    posts: [postSchema] // postSchema must be defined first
+});
+let User = mongoose.model("aUser", userSchema);
+
+// let newUser = new User({
+//     email: "test2test1@gmail.com",
+//     name: "Theory Craft"
+// });
+//
+// newUser.posts.push({
+//     title: "How to become successful?",
+//     post: "Just do it."
 // });
 //
 // newUser.save(function (err, user) {
-//     if(err){
-//         console.log(err);
-//     } else {
-//         console.log(user);
-//     }
+//    if(err){
+//        console.log(err);
+//    } else {
+//        console.log(user);
+//    }
 // });
 
-// let newPost = new postModel({
-//     title: "Where Is Occupy’s Free Internet Champion?",
-//     post: "Five years ago, Motherboard released a doc on Occupy Wall Street hacktivists trying to build a distributed, ad hoc mesh network from the ground up. We caught up with one of them."
-// });
-//
-// newPost.save(function (err, user) {
-//     if(err){
-//         console.log(err);
-//     } else {
-//         console.log(user);
-//     }
-// });
+User.findOne({name: "Theory Craft"}, function (err, user) {
+    if(err){
+        console.log(err);
+    } else {
+        // user.posts.push({
+        //     title: "50 Top Traits of Successful Minds",
+        //     post: "Just do it."
+        // });
+        // user.save(function (err, user) {
+        //    if(err){
+        //        console.log(err);
+        //    } else {
+        //        console.log(user);
+        //    }
+        // });
+        console.log(user);
+    }
+});
 
